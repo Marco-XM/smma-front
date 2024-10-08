383561214c742f7cdb263c4c4e16dd3a9d23deda
marco <marcoelgawli66@gmail.com>
2024-10-08 14:13:49 +0300

    update

--- my-vite-react-app/src/pages/homePage.jsx
+++ my-vite-react-app/src/pages/homePage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import image from '../assets/northcost-3.jpg';
+import imagetop from '../assets/northcost-33.png';
 import image2 from '../assets/20210402_225934.jpg';
 import image3 from '../assets/20230202_025321.jpg';
 import NavBar from '../components/NavBar';
@@ -9,12 +10,19 @@ const HomePage = () => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       const sections = document.querySelectorAll('.parallax');
+      const topImage = document.querySelector('.top-image');
 
       sections.forEach((section) => {
         const speed = 0.25; // Adjust the speed factor to make the background scroll slower
         const offset = scrollPosition * speed;
         section.style.backgroundPositionY = `${offset}px`;
       });
+
+      if (topImage) {
+        const topImageSpeed = 0.25; // Adjust the speed factor for the top image
+        const topImageOffset = scrollPosition * topImageSpeed;
+        topImage.style.transform = `translateY(${topImageOffset}px)`;
+      }
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -28,9 +36,15 @@ const HomePage = () => {
     <>
       <div className='grid grid-rows-3'>
         <div
-          className='section h-screen bg-cover bg-center flex parallax'
+          className='section h-screen bg-cover bg-center flex parallax relative overflow-hidden'
           style={{ backgroundImage: `url(${image})` }}
         >
+          <img
+            src={imagetop}
+            alt="Top Overlay"
+            className='top-image over absolute bottom-0 left-0 w-full h-full object-cover object-center shadow-image'
+            style={{ zIndex: 1 }}
+          />
           <div className='fixed w-full flex justify-between'>
             <div className='relative w-full'>
               <div className='absolute p-5 px-20 rounded-br-full text-white'>
@@ -44,10 +58,10 @@ const HomePage = () => {
               <div className='absolute right-0 p-5 px-16 text-white rounded-bl-full'>Sign Up</div>
             </div>
           </div>
-          <div className='self-center m-10'>
-            <div className='text-white flex flex-col ml-40'>
+          <div className='self-center w-full flex justify-center translate-x-48 welcome-container'>
+            <div className='text-white flex flex-col'>
               <h1 className='text-9xl'>Welcome.</h1>
-              <h3 className='text-3xl self-center typing-effect w-fit'>Grow Your Business Quickly</h3>
+              <h3 className='text-3xl self-center typing-effect w-fit' style={{ zIndex: 2 }}>Grow Your Business Quickly</h3>
             </div>
           </div>
         </div>
